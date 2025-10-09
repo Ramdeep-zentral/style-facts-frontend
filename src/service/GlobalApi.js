bb70e84757d4d32097756bc8e52d3fd8c3e07154
Ramdeep-zentral <103029331+Ramdeep-zentral@users.noreply.github.com>
2025-10-09 23:04:31 +0200

    Refactor GlobalApi.js for improved readability and consistency

--- src/service/GlobalApi.js
+++ src/service/GlobalApi.js
@@ -1,26 +1,41 @@
+import axios from "axios";
 
-import axios from 'axios';
+const API_KEY = process.env.NEXT_PUBLIC_STRAPI_API;
 
-const API_KEY= process.env.NEXT_PUBLIC_STRAPI_API;
+const baseURL =
+  process.env.NEXT_PUBLIC_BACKEND_API_URL && process.env.NEXT_PUBLIC_BACKEND_API_URL !== ""
+    ? process.env.NEXT_PUBLIC_BACKEND_API_URL
+    : "http://localhost:1337/api";
+
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:1337/api',
-       headers:{
-        'Content-Type':'application/json',
-        'Authorization':`Bearer ${API_KEY}`
-    }
+  baseURL: baseURL,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
 });
 
-const GetBlogs = () => axiosClient.get('/blogs?populate=*');
-const GetCategories = () => axiosClient.get('/categories');
-const GetTags = () => axiosClient.get('/tags');
-const GetBlogsByCategory = (category) => axiosClient.get(`/blogs?filters[categories][slug][$eq]=${category.toLowerCase()}&populate=*`);
-const GetSingleBlog = (slug) => axiosClient.get(`/blogs?filters[slug][$eq]=${slug}&populate=*`);
-const SubmitBlog = (blogData) => axiosClient.post('/blogs', blogData);
+const GetBlogs = () => axiosClient.get("/blogs?populate=*");
+const GetCategories = () => axiosClient.get("/categories");
+const GetTags = () => axiosClient.get("/tags");
+const GetBlogsByCategory = (category) =>
+  axiosClient.get(
+    `/blogs?filters[categories][slug][$eq]=${category.toLowerCase()}&populate=*`
+  );
+const GetSingleBlog = (slug) =>
+  axiosClient.get(`/blogs?filters[slug][$eq]=${slug}&populate=*`);
+const SubmitBlog = (blogData) => axiosClient.post("/blogs", blogData);
 const UploadImage = (formData) =>
-  axiosClient.post('/upload', formData, {
-    headers: { 'Content-Type': 'multipart/form-data' }
+  axiosClient.post("/upload", formData, {
+    headers: { "Content-Type": "multipart/form-data" },
   });
 
-
-
-export default{ GetBlogs, GetCategories, GetTags, GetBlogsByCategory, GetSingleBlog, SubmitBlog, UploadImage };
+export default {
+  GetBlogs,
+  GetCategories,
+  GetTags,
+  GetBlogsByCategory,
+  GetSingleBlog,
+  SubmitBlog,
+  UploadImage,
+};
